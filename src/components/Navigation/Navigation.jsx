1249d9350fb61e40ab6c63af00fb19c76c3fa491
akashzone <akashnadar.dev@gmail.com>
2025-12-19 12:57:19 +0530

    add navigation bar component

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,9 +1,21 @@
 
-
+import styles from './Navigation.module.css'
 const Navigation = () => {
-  return (
-    <div>Navigation</div>
-  )
+    // console.log(styles)
+    return (
+        <>
+            <nav className={`${styles.navigation} container`}>
+                <div>
+                    <img className={styles.logo} src="logo.png" alt="Logo" />
+                </div>
+                    <ul>
+                        <li className={styles.navLinks}>Home</li>
+                        <li className={styles.navLinks}>About</li>
+                        <li className={styles.navLinks}>Contact</li>
+                    </ul>
+            </nav>
+        </>
+    )
 }
 
 export default Navigation
